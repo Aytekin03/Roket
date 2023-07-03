cc551fb9b7d64715295a825bedd1f2eaa1ce54ed
kadir <gonultaskdr@gmail.com>
2023-07-03 14:00:48 +0300

    carpısma.cs ye açıklama satırı eklendi

--- Assets/Script/Carpisma.cs
+++ Assets/Script/Carpisma.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; 
 
+// Bunu yazan tosun
+
 public class Carpisma : MonoBehaviour
 {
     [SerializeField] float levelLoadDelay = 2f;
